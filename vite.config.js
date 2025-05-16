bc824654d7566c63ac42a6938d9e7df7ce897baa
hieucodekk <hieudo149204@gmail.com>
2025-05-16 22:19:46 +0700

    22

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/bt-react/", // <-- rất quan trọng để GitHub Pages hoạt động đúng
   plugins: [react()],
-})
+});
